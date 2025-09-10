6e4cd3e4edc6481427aee427e7f17fa8e77c2948
Keely-Ann <ST10085428@vcconnect.edu.za>
2025-09-10 11:52:03 +0200

    Changed the styling of the Index building image and added the new IndexBanner image into the Images folder. Added references for all pages.

--- PROG7312_POE_PART1/Controllers/HomeController.cs
+++ PROG7312_POE_PART1/Controllers/HomeController.cs
@@ -42,14 +42,25 @@ namespace PROG7312_POE_PART1.Controllers
 
         public IActionResult ContactUs()
         {
-            // List of Team Members Displayed on the Contact Us Page
+            // List of Team Members Displayed on the Contact Us Page 
             var teamMembers = new List<TeamMembers>
             {
+                //Team Member 1 (Freepik,2025)
                 new TeamMembers { Name = "MR. Luvuyo Magalela",Position = "Executive Director", Email="edee@mandelametro.gov.za",Image="teammember1.jpg"},
+                
+                //Team Member 2 (Freepik,2025)
                 new TeamMembers { Name = "MR. Henry Williams",Position = "Deputy Director: Beaches & Resorts", Email="hwilliams@mandelametro.gov.za",Image="teammember2.jpg"},
+                
+                //Team Member 3 (Freepik,2025)
                 new TeamMembers { Name = "MRS. Lesley Dunderdale",Position = "Director : Electricity & Energy", Email="ldunderdale@mandelametro.gov.za",Image="teammember3.jpg"},
+                
+               //Team Member 4 (Freepik,2025)
                 new TeamMembers { Name = "MR. Luke Deliwe",Position = "Manager: Air & Noise Pollution", Email="bhumani@mandelametro.gov.za",Image="teammember4.jpg"},
+                
+                //Team Member 5 (Freepik,2025)
                 new TeamMembers { Name = "MRS. Phumeza Marotya",Position = "Manager: Animal & Pest Control", Email="pmarotya@mandelametro.gov.za",Image="teammember5.jpg"},
+                
+                //Team Member 6 (Freepik,2025)
                 new TeamMembers { Name = "MS. Rebecca Abioye",Position = "Emergency Medical Services (EMS) Co-ordinator", Email="rabioye@mandelametro.gov.za",Image="teammember6.jpg"}
             };
             // returning the list of teamMembers
